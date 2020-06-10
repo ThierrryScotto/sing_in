c5a8e3ff08ef3b571ad84ce29f56a099981886fe
thierry <thierry_192@outlook.com>
2020-06-10 03:18:49 -0300

    create function to check login

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,8 +1,22 @@
+const User = require("../models/user")
+
 module.exports = {
-  createUser(req, res) {
-    //cria o user
-  },
   checkLogin(req, res) {
-    //check user and password
+
+  //check user and password
+
+  let { password, email } = req.body.login;
+
+  User.findOne({
+    where: {email: email, password: password},
+  }).then(success => {
+    if (success)
+      res.send({ success: true})
+    else 
+      res.send({ success: flase, error: "Usuário não tem cadastro"})
+  })
+  .catch(err => {
+    console.log(err);
+  })
   }
 }
