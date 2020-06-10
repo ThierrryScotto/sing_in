afd9f1ba122b0ea49357aa68f6f3abc63e558370
thierry <thierry_192@outlook.com>
2020-06-10 03:04:02 -0300

    create route createUser

--- server/src/router.js
+++ server/src/router.js
@@ -1,2 +1,8 @@
 const app = require("./server/app")
+const UserController = require("./controllers/user");
 
+app.get("/", (req, res, next) => {
+  res.send("You are connected!")
+})
+
+app.post("/createUser", UserController.createUser);
