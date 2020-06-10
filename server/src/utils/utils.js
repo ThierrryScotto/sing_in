f7de41823a581867c7fa3ee7dffd423891474e79
thierry <thierry_192@outlook.com>
2020-06-09 23:11:34 -0300

    add functions utils and folder

---
+++ server/src/utils/utils.js
@@ -0,0 +1,12 @@
+module.exports = {
+  verifyToken(res, req, next) {
+    //verifica se as informações estão vindo do front end que eu quero
+  },
+  decrypt(res, req, next) {
+    //verifica decript as informações encripitadass
+  },
+  encrypt(res, req, next) {
+    //verifica encrypt as informações que eu quero
+  },
+
+}
