4d4ecec53c7e574b09f6bb55b6827a21c7af728c
thierry <thierry_192@outlook.com>
2020-06-10 03:03:41 -0300

    create controller user

---
+++ server/src/controllers/user.js
@@ -0,0 +1,31 @@
+const User = require("../models/user")
+
+module.exports = {
+  async createUser(req, res) {
+    //chamar a função que vai decripitar as informarções 
+    let { name, password, email } = req.body.user;
+
+    await User.create({
+      name,
+      password,
+      email,
+    }).then(() => {
+      res.send({ success: true })
+    }).catch(err => {
+      res.send({ success: true, error: "Erro ao cadastrar usuário" })
+      console.log(err)
+    })
+  },
+
+  validEmail(email) {
+    User.findOne({
+      where: {email: email},
+    }).then(success => {
+      if (success) return false
+      else return true;
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}
