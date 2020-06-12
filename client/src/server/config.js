07bee191d9d408531723c85735ac14ebd4854d00
thierry <thierry_192@outlook.com>
2020-06-11 22:36:03 -0300

    fix bug config

--- client/src/server/config.js
+++ client/src/server/config.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  // baseURL: process.env.BASE_URL,
+  baseURL: 'http://localhost:3000/'
 })
